e29cd237eb84556408c853c52b6862b26742afb0
Blake Burbach <cras.bburbach@gmail.com>
2018-04-20 16:08:46 -0500

    indexjs ready. Starting on appjs routes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
@@ -13,6 +13,33 @@ import axios from 'axios';
 // Set redux-saga's middleWare function to a variable to use more easily
 // with other functions
 const sagaMiddleware = createSagaMiddleware();
+
+// rootSaga will be the hub and initializing of all Saga functions.
+// Any dispatch call will look here first. If it's not a saga, it will
+// be a reducer function and go there instead.
+function* rootSaga() {
+    console.log('rootSaga Loaded');
+}// end rootSaga
+
+// reducer responsible for the state of newReflection page
+const newReflection = (state={}, action) => {
+    console.log('newReflection loaded')
+    return state
+}
+
+
+// storeInstance will be the only instance of reduxState in this project.
+// This is where we can reduxState what reducers it will be in control of
+// and what middleware it will be using.
+const storeInstance = createStore(
+    // store can only run one reducer at a time, so combineReducers is used to tell
+    // redux store that it will be handling more than one reducer by putting them all
+    // in an object
+    combineReducers({
+        newReflection
+    }),
+    applyMiddleware(sagaMiddleware, logger)
+) // end storeInstance
 
 
 // initialize the rootSaga function which will authenticate
