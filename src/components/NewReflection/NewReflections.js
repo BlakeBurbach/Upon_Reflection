369488cf2c38dbf644d477a74d7db4dab5ee2697
Blake Burbach <cras.bburbach@gmail.com>
2018-04-22 13:32:07 -0500

    added text areas to cards for input of reflection. added stlying to them as well

--- src/components/NewReflection/NewReflections.js
+++ src/components/NewReflection/NewReflections.js
@@ -1,56 +1,50 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField'
-import Card from 'material-ui/Card'
-import './NewReflections.css'
-// import SelectField from 'material-ui/SelectField';
-// import MenuItem from 'material-ui/MenuItem';
-
+import Card from 'material-ui/Card';
+import './NewReflections.css';
+import Button from 'material-ui/Button';
 
 const styles = {
     customWidth: {
-      width: 150,
+        width: 150,
     },
-  };
+};
 
 class NewReflections extends Component {
     state = {
         value: 1,
-      };
-    
-    handleChange = (event, index, value) => this.setState({value});
+    };
 
-    render(props){
+    handleChange = (event, index, value) => this.setState({ value });
+
+    render(props) {
         return (
             <div className="cardHolder">
                 <h2>
                     Add New Reflections
                 </h2>
                 <Card className="inputCard">
-                {/* <SelectField
-                    floatingLabelText="Frequency"
-                    value={this.state.value}
-                    onChange={this.handleChange}>
-                    <MenuItem value={1} primaryText="Never" />
-                    <MenuItem value={2} primaryText="Every Night" />
-                    <MenuItem value={3} primaryText="Weeknights" />
-                    <MenuItem value={4} primaryText="Weekends" />
-                    <MenuItem value={5} primaryText="Weekly" />
-                </SelectField> */}
-                <TextField
-                    label="Enter Your Memory"
-                    fullWidth={true}
-                    rows={2}
-                    rowsMax={4}
-                    {...props}
-                />
-
-                <TextField
-                    label="Enter Your Memory"
-                    fullWidth={true}
-                    rows={2}
-                    rowsMax={4}
-                    {...props}
-                />
+                <div className="reflectionContainer">
+                    <label for="topic">
+                          <strong> Topic </strong>
+                    </label>
+                    <br/>
+                    <textarea name="topic" id="topicArea">
+                    </textarea>
+                </div>
+                    <br />
+                <div className="reflectionContainer">
+                    <label for="description">
+                          <strong> Reflection </strong>
+                    </label>
+                        <br />
+                    <textarea name="description" id="descriptionArea">
+                    </textarea>
+                </div>
+                    <br />
+                    <div className="buttonHolder">
+                        <Button className="rememberButton">Remember</Button>
+                    </div>
                 </Card>
             </div>
         )
